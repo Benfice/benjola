2531e894ee9c98515fd8487ed9d0bd61547c7107
Benoit Freulon <bfreulon@orange.fr>
2022-09-09 17:33:02 +0200

    User Registration + fix on CustomUserCreationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,11 +3,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
 class CustomUserCreationForm(UserCreationForm):
-    model = get_user_model()
-    fields = (
-        "email",
-        "username",
-    )
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "email",
+            "username",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
--- benjola/settings.py
+++ benjola/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+
+# Redirect after logging in and logging out
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
