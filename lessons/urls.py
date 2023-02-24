96a213aa63a52861197a32276514df09200d208c
Benoit Freulon <bfreulon@orange.fr>
2023-02-24 09:24:00 +0100

    Editing and deleting lessons

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import LessonListView, LessonDetailView
+from .views import (
+    LessonListView, 
+    LessonDetailView, 
+    LessonCreateView,
+    LessonUpdateView,
+    LessonDeleteView,
+)
 
 urlpatterns = [
     path("", LessonListView.as_view(), name="lesson_list"),
     path("<uuid:pk>/", LessonDetailView.as_view(), name="lesson_detail"),
+    path("new/", LessonCreateView.as_view(), name="lesson_new"),
+    path("<uuid:pk>/edit/", LessonUpdateView.as_view(), name="lesson_edit"),
+    path("<uuid:pk>/delete/", LessonDeleteView.as_view(), name="lesson_delete"),
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Lesson
 
 
@@ -12,3 +14,21 @@ class LessonDetailView(DetailView):
     model = Lesson
     context_object_name = "lesson"
     template_name = "lessons/lesson_detail.html"
+
+
+class LessonCreateView(CreateView):
+    model = Lesson
+    template_name = "lessons/lesson_new.html"
+    fields = ["title", "author", "body"]
+
+
+class LessonUpdateView(UpdateView):
+    model = Lesson
+    template_name = "lessons/lesson_edit.html"
+    fields = ["title", "body"]
+
+
+class LessonDeleteView(DeleteView):
+    model = Lesson
+    template_name = "lessons/lesson_delete.html"
+    success_url = reverse_lazy("lesson_list")
